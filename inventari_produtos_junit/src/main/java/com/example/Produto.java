c31cab6fb8281fb79c72db56f0c894d7330e5577
kelvenbrito <kelvenbrito14@gmail.com>
2024-07-29 19:20:45 -0300

    29/07/24

---
+++ inventari_produtos_junit/src/main/java/com/example/Produto.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+
+public class Produto {
+    //atributos
+    private int id;
+    private String nome;
+    private String fabricante;
+    private double preco;
+    private int quantidade;
+
+    
+
+
+
+
+}
