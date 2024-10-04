ec288e08b3035018f6440ec234004ba0ccf69adc
kelvenbrito <kelvenbrito14@gmail.com>
2024-10-03 21:56:31 -0300

    03/10/24

--- javaProjects/gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
+++ javaProjects/gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
@@ -3,18 +3,21 @@ package com.example;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculadoraSimples extends JPanel {
+    // ATRIBUTOS
     JTextField displaySimples;
     String[] botoes = { "7", "8", "9", "/",
             "4", "5", "6", "*",
             "1", "2", "3", "-",
             "0", "=", "+", "C" };
 
-    // Construtor
+    // CONSTRUTOR
     public CalculadoraSimples() {
         super(new BorderLayout());
         displaySimples = new JTextField();
@@ -22,39 +25,66 @@ public class CalculadoraSimples extends JPanel {
         displaySimples.setEditable(false);
         this.add(displaySimples, BorderLayout.NORTH);
 
-        // Criar um painel para os botões
+        // Criar um Painel para os botões
         JPanel painelBotoes = new JPanel(new GridLayout(4, 4, 3, 3));
         for (String textoBotoes : botoes) {
             JButton botao = new JButton(textoBotoes);
-            // adicionar ação dos botoes
-            botao.addActionListener(new ActionListener() {
-                
-            });
+            botao.addActionListener(new Handler1());
             painelBotoes.add(botao);
         }
-            this.add(painelBotoes, BorderLayout.CENTER);
-      
+        this.add(painelBotoes, BorderLayout.CENTER);
     }
 
-    //altera valor display
- public void setDisplay(String texto) {
-     this.displaySimples.setText(texto);
+    // altera valor display
+    public void setDisplay(String texto) {
+        this.displaySimples.setText(texto);
     }
 
-    //pega valor display
-    public String getDisplay(){
+    // pega valor display
+    public String getDisplay() {
         return displaySimples.getText();
-   
+    }
 
-    public class SimpleActionListener implements ActionListener{
+    public class Handler1 implements ActionListener {
+        double valorAtual;
+        String operador;
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+
+            String comando = e.getActionCommand();
+
+            
+            if (comando.matches("\\d")) {
+                displaySimples.setText(displaySimples.getText() + comando);
+            } else if (comando.matches("[+\\-*/]")) {
+                valorAtual = Double.parseDouble(displaySimples.getText());
+                operador = comando;
+                displaySimples.setText("");
+            } else if (comando.equals("=")) {
+                double segundoValor = Double.parseDouble(displaySimples.getText());
+                double resultado = 0;
+
+                switch (operador) {
+                    case "+":
+                        resultado = valorAtual + segundoValor;
+                        break;
+                    case "-":
+                        resultado = valorAtual - segundoValor;
+                        break;
+                    case "*":
+                        resultado = valorAtual * segundoValor;
+                        break;
+                    case "/":
+                        resultado = valorAtual / segundoValor;
+                        break;
+                }
+                displaySimples.setText(String.valueOf(resultado));
+            } else if (comando.equals("C")) {
+                displaySimples.setText("");
+                valorAtual = 0;
+            }
+
         }
     }
-        
-    }
-
 }
